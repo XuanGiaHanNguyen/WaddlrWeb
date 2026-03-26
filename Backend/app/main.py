9617c7cc6c0620e8bad6dec93baf2fa729905249
XuanGiaHanNguyen <han316@usf.edu>
2026-03-26 13:01:57 -0400

    freshstart due to tailwind react issue

--- Backend/app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from app.routes import predict
-
-app = FastAPI()
-
-app.include_router(predict.router)
--- Frontend/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="w-nav">
-      <div className="nav-logo">Waddlr.</div>
-      <a href="#download" className="nav-cta">Get the App</a>
-    </nav>
-  );
-}
- 
